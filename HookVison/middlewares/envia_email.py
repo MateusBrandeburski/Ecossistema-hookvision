2a834cd2864f4da4debcaf4ab16da0f84785925e
Mateus Brandeburski <mateus.brandeburski92@gmail.com>
2024-12-13 21:34:53 -0300

    add esquema de envio de email com preteção contra flood

---
+++ HookVison/middlewares/envia_email.py
@@ -0,0 +1,25 @@
+import httpx
+import sqlite3
+from datetime import datetime, timedelta
+
+def enviar_dados_para_api(ip, user_agent):
+    with sqlite3.connect('ips.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS ips_enviados (ip TEXT PRIMARY KEY, hora_envio DATETIME)")
+    
+        cursor.execute('SELECT hora_envio FROM ips_enviados WHERE ip = ?', (ip,))
+        resultado = cursor.fetchone()
+        
+        if resultado:
+            hora_envio = datetime.strptime(resultado[0], '%Y-%m-%d %H:%M:%S')
+            if datetime.now() - hora_envio < timedelta(hours=3):
+                print(f"IP {ip} já enviou um e-mail nas últimas 3 horas. Não será enviado novamente.")
+                return  # Não envia e-mail
+
+        cursor.execute('INSERT OR REPLACE INTO ips_enviados (ip, hora_envio) VALUES (?, ?)', 
+                       (ip, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+        with httpx.Client() as client:
+            response = client.post(f'http://localhost:8000/api/enviar-email?ip={ip}&user_agent={user_agent}')
+            print(f"Status da resposta: {response.status_code}")
+            print(f"Resposta da API: {response.text}")
--- HookVison/routes/home/home.py
+++ HookVison/routes/home/home.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect, url_for
-from controllers.dashboard.tabela import Tabela
+from flask import Blueprint, render_template, session, redirect, url_for, request
+from middlewares.envia_email import enviar_dados_para_api
 
 home = Blueprint('home', __name__)
 
@@ -7,5 +7,12 @@ home = Blueprint('home', __name__)
 def index():
     if not session.get('usuario_logado', None):
         return redirect(url_for('login.index'))
+    
     language = session.get('lang', 'en')
+
+    ip_cliente = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+
+    enviar_dados_para_api(ip_cliente, user_agent)
+
     return render_template('home/dashboard/dashboard.html', lang=language)
